b9d5e60ef166e75fdaf9c6b47fcdfd661199b4e8
unknown <my05.abad@gmail.com>
2018-02-05 19:08:59 -0500

    cambio foter

--- pie.php
+++ pie.php
@@ -7,18 +7,18 @@
 	width: 100%;
 	position: fixed;
 	left: 0px;
-	bottom: 0px; 
+	bottom: 0px;
     }
     hr{
         color: #eee;
         box-shadow: -2px 0px 10px 7px;
-        
+
     }
 </style>
 <footer>
-        
+
     <hr>
-    <p style="text-align: center">&COPY; 2018 Todo los derechos reservados. |  Diseñado por <a href="https://twitter.com/miltabad" target="_blank">Michael Dionicio Abad</a></p>
+    <p style="text-align: center">&COPY; 2018 Todo los derechos reservados. |  Desarrollado por <a href="https://twitter.com/miltabad" target="_blank">Michael Dionicio Abad</a></p>
 </footer>
 </body>
 </html>
